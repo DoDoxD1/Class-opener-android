910dcc3694c4351a7145c71d79f2ce3dfc1f290f
aunuj <aunujain54@gmail.com>
2021-03-16 19:39:14 +0530

    Time Table Zoom added

--- app/src/main/java/com/example/myapplication/TimeTable.java
+++ app/src/main/java/com/example/myapplication/TimeTable.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
 
 public class TimeTable extends AppCompatActivity {
 
